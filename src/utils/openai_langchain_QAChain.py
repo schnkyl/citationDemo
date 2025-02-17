31089ad1a87cb5a6d93e5096b2530ea39a68ed97
schnkyl <schnkyl@gmail.com>
2025-02-17 08:29:18 -0500

    Notion test 2

--- src/utils/openai_langchain_QAChain.py
+++ src/utils/openai_langchain_QAChain.py
@@ -10,6 +10,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter  # type: ign
 from langchain.chains import create_retrieval_chain # type: ignore
 from langchain.chains.combine_documents import create_stuff_documents_chain  # type: ignore
 from langchain_community.document_loaders import PyPDFLoader  # type: ignore
+from langchain_community.vectorstores import FAISS # type: ignore
 from langchain_core.prompts import ChatPromptTemplate  # type: ignore
 from langchain.chat_models import init_chat_model # type: ignore
 from langchain_core.documents import Document # type: ignore
@@ -45,12 +46,15 @@ system_prompt = (
 loader = PyPDFLoader('/Users/kyle/Documents/BreezeRFP/citationDemo/src/Boston - Mobile App Development RFP.pdf')
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200
-)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(
-    documents=splits, embedding=OpenAIEmbeddings()
-)
+            chunk_size=1000,
+            chunk_overlap=200,  # Recommended to have some overlap between chunks
+            length_function=len,
+            separators=["\n\n", "\n", " ", ""]
+        )
+
+chunks = text_splitter.split_documents(docs)
+
+vectorstore = FAISS.from_documents(chunks, OpenAIEmbeddings())
 retriever = vectorstore.as_retriever()
         
 prompt = ChatPromptTemplate.from_messages(
